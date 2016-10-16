74f318516e0c65d54bd80e41917c81efe84cc05d
pvazteixeira <pvt@mit.edu>
2016-10-16 12:33:53 -0400

    load/save config; fixed-threshold segmentation

--- example.py
+++ example.py
@@ -9,34 +9,27 @@ Short example on the use of the multibeam module
 
 # instantiate a sonar object
 didson = Sonar();
-
-# set its parameters
-didson.fov = np.deg2rad(28.8)
-didson.num_beams = 96
-didson.num_bins = 512
-
-# update the sonar window parameters
-didson.resetWindow(2.25, 11.75) 
+didson.loadConfig('data/DIDSON/didson.json')
 
 # load image
 ping = cv2.imread('data/DIDSON/tuna_can.png',cv2.CV_LOAD_IMAGE_GRAYSCALE)
 
 # show original image
 plt.figure()
-plt.subplot(1,3,1)
+plt.subplot(1,4,1)
 plt.imshow(ping)
 
 # convert original image to Cartesian
 ping_cart = didson.toCart(ping) 
-plt.subplot(1,3,2)
+plt.subplot(1,4,2)
 plt.imshow(ping_cart)
 
 # define sonar's psf (sonar-specific)
-beam_pattern_indices = np.array([0,8,16,24,32,40,48,56,64,72,80,88])
-beam_pattern =  np.zeros((1,96))
-beam_pattern[0, beam_pattern_indices] = [24,24,24,27,32,40,70,40,32,27,24,24]
-psf = (1.0/np.sum(beam_pattern))*beam_pattern
-didson.psf = psf
+# beam_pattern_indices = np.array([0,8,16,24,32,40,48,56,64,72,80,88])
+# beam_pattern =  np.zeros((1,96))
+# beam_pattern[0, beam_pattern_indices] = [24,24,24,27,32,40,70,40,32,27,24,24]
+# psf = (1.0/np.sum(beam_pattern))*beam_pattern
+# didson.psf = psf
 
 # deconvolve
 ping_deconv = didson.deconvolve(ping)
@@ -45,9 +38,19 @@ ping_deconv = didson.deconvolve(ping)
 ping_deconv_cart = didson.toCart(ping_deconv)
 
 # show enhanced image
-plt.subplot(1,3,3)
+plt.subplot(1,4,3)
 plt.imshow(ping_deconv_cart)
+
+print np.amax(ping_deconv_cart)
+# segment ping
+binary_ping = didson.segment(ping_deconv,100.0)
+print np.amin(binary_ping), np.amax(binary_ping)
+binary_ping_cart = didson.toCart(binary_ping)
+plt.subplot(1,4,4)
+plt.imshow(binary_ping_cart)
 
 # display results
 plt.show()
 
+
+cv2.imwrite('ping.png',ping_deconv_cart)
--- multibeam.py
+++ multibeam.py
@@ -1,30 +1,55 @@
-import cv2
+import cv2 
 import numpy as np
+import json
  
 class Sonar:
 
   def __init__(self):
+    # some sane defaults
     self.min_range = 1.0
     self.max_range = 10.0
-    # beamwidth   # 
-    # fov          # need this to deal with sonars with overlapping beams ? handle this in example
     self.fov = np.deg2rad(90.0)
     self.num_beams = 128
     self.num_bins = 512
-    # PSF    - 
     self.psf = np.ones((1,1)) 
     self.noise = 0.01
 
-    # look-up table used to speed up conversion
+    # look-up table used to speed up conversion from polar to cartesian
     self.row_cart =[]
     self.col_cart =[]
     self.row_polar =[]
     self.col_polar = []
     self.__computeLookUp__(0.02) 
 
+  def loadConfig(self, cfg_file='sonar.json'):
+    with open(cfg_file) as sonar_config_file:
+      cfg = json.load(sonar_config_file)
+    self.min_range = cfg['min_range']
+    self.max_range = cfg['max_range']
+    self.fov = cfg['fov']
+    self.num_beams = cfg['num_beams']
+    self.num_bins = cfg['num_bins']
+    self.psf = np.hstack(cfg['psf']) 
+    self.psf.shape = (1,96)
+    self.noise = cfg['noise']
+    self.__computeLookUp__(0.02)
+
+  def saveConfig(self, cfg_file='sonar.json'):
+    cfg={}
+    cfg['min_range'] = self.min_range
+    cfg['max_range'] = self.max_range
+    cfg['fov'] = self.fov
+    cfg['num_beams'] = self.num_beams
+    cfg['num_bins'] = self.num_bins
+    cfg['psf'] = self.psf
+    cfg['noise'] = self.noise
+    with open(cfg_file, 'w') as fp:
+      json.dump(cfg, fp, sort_keys=True, indent=2)
+
   def __computeLookUp__(self, resolution):
     """Compute lookup table used in polar to cartesian conversion
-    """
+
+   """
     bin_length = (self.max_range - self.min_range)/(self.num_bins + 0.0)
     beamwidth  = (self.fov)/(self.num_beams+0.0)
 
@@ -95,7 +120,12 @@ class Sonar:
     Keyword arguments:
     ping - the sonar scan, in polar representation
     width - the desired with of the Cartesian representation (default: 320)
-    """
+    Note: some conversion performance values as a function of resolution: 
+      resolution | conversion time
+      0.01 m/px    9ms
+      0.02 m/px    2ms
+      0.03 m/px    1ms
+   """
     image = np.zeros((self.height, self.width)) 
     ping[0,0] = 0
     image[self.row_cart.flatten(), self.col_cart.flatten()] = ping[self.row_polar.flatten(), self.col_polar.flatten()]
@@ -139,3 +169,36 @@ class Sonar:
     result*=255.0
     return result.astype(np.uint8)
 
+  def segment(self, ping, threshold):
+    """ Segments the image using a fixed threshold
+    
+    Keyword arguments:
+    ping 
+    threshold - the segmenting threshold (0-1 range)
+    
+    Note:
+    Return image type is the same as the input's.
+    """
+    
+    ping_binary = ping.astype(np.float64)
+    ping_binary[ping_binary<threshold]=0
+    ping_binary[ping_binary>0]=1.0
+    
+    return ping_binary.astype(ping.dtype)
+
+  def getReturns(self, segmented_ping):
+    """ Computes the location of the first return along the beam for all the beams
+
+    Keyword arguments:
+    segmented_ping - the segmented ping/scan (e.g. as provided by segment())
+    Output:
+    returns - a 2D vector containing the (x,y) positions of the return for each of the sonar beams;
+              beams for which no return was found will have (-1,-1) as the return position.
+    """
+    # get the bin location for each beam
+    # convert (bin, beam ) to (r,theta)
+    # convert (r, theta) to (x,y)
+    positions = np.zeros((2,np.num_beams))
+    
+    # (x,y) = cv2.polarToCart()
+    return positions
